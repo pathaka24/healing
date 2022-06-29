29a55933569f90d73b8577f80343bb82113d317a
hyo joo <pathaka24@gmail.com>
2022-06-30 01:36:05 +0530

    new js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Navbar from '../components/navbar/navbar'
 import HomePage from '../components/homeBanner/home'
 import Container from '../components/container/container'
 import Banner from '../components/banner/banner'
+import Service from '../components/service/service'
  
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
            <HomePage  imgUrl='/static/banner.jpg'/>
            <Container /> 
            <Banner imgUrl='/static/proudly-1.jpg' pattern='/static/pattern-1.png' />
+           <Service />
     </div>
   )
 }
